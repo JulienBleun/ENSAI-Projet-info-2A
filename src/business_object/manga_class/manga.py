ce141b2ebd71d1ae8a80638b17a9083b90d5b6c3
Léo <leo.gruchociak2@gmail.com>
2024-10-11 12:36:07 +0200

    classes manga

---
+++ src/business_object/manga_class/manga.py
@@ -0,0 +1,12 @@
+class Manga():
+
+    def __init__(
+            self,
+            titre : str,
+            auteur : str,
+            id_manga : int
+            ) :
+
+        self.titre = titre
+        self.auteur = auteur
+        self.id_manga = id_manga
