13cb111b2563f76805971bf023824efc5f6cc63f
inesbm17 <benmoussa.ines11@gmail.com>
2024-10-21 16:47:16 +0200

    classe avis collection

--- src/business_object/avis.py
+++ src/business_object/avis.py
@@ -1,12 +1,22 @@
-class avis():
+class AvisCollection():
+        """
+    Classe représentant un avis spécifique à une collection.
 
-    def __init__(self,
-                 id_avis: int,
-                 id_objet_avis: int,
-                 contenu: str,
-                 note: int):
+    Cette classe hérite de `AbstractAvis` et ajoute un attribut supplémentaire pour l'identifiant
+    de la collection à laquelle l'avis est lié. 
 
-        self.id_avis = id_avis
-        self.id_objet_avis = id_objet_avis
-        self.contenu = contenu
-        self.note = note
+    Attributs supplémentaires :
+    ---------------------------
+    id_collection : int
+        Identifiant unique de la collection associée à cet avis.
+    """
+
+        def __init__(self,
+                     id_avis: int,
+                     id_utilisateur: int,
+                     commentaire: str,
+                     note: int,
+                     id_collection: int):
+
+            super().__init__(id_avis, id_utilisateur, commentaire, note)
+            self.id_collection = id_collection
