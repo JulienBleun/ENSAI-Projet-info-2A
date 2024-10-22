3ee2345aa873055034db524bee361cc1c83c26bc
inesbm17 <benmoussa.ines11@gmail.com>
2024-10-22 10:19:32 +0200

    git

--- src/business_object/abstract_avis.py
+++ src/business_object/abstract_avis.py
@@ -1,15 +1,29 @@
 from abc import ABC
+ """
+    Classe abstraite représentant un avis .
 
+    Parametres :
+    -----------
+    id_avis : int
+        Identifiant de l'avis.
+    id_utilisateur : int
+        Identifiant de l'utilisateur qui a laissé l'avis.
+    commentaire : str
+        Le texte du commentaire laissé par l'utilisateur.
+    note : int
+        La note attribuée à l'avis
+
+    """
 
 class AbstractAvis(ABC):
 
     def __init__(self,
                  id_avis: int,
                  id_objet_avis: int,
-                 contenu: str,
+                 commentaire: str,
                  note: int):
 
         self.id_avis = id_avis
         self.id_objet_avis = id_objet_avis
-        self.contenu = contenu
+        self.commentaire = commentaire
         self.note = note
