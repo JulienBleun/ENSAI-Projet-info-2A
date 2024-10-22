67689373f8c51f0df037d49ff9523bf30b0df500
Julien Bleunven <julien.bleunven@eleve.ensai.fr>
2024-10-22 09:29:23 +0200

    a

--- src/business_object/abstract_collection.py
+++ src/business_object/abstract_collection.py
@@ -1,7 +1,7 @@
-from abc import ABC, abstractmethod
+from abc import ABC
+
 
 class AbstractCollection(ABC):
     def __init__(self, id_utilisatuer: int, id_collection : int):
         self.id_utilisateur = id_utilisateur
         self.id_collection = id_collection
-
