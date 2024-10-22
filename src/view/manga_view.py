8eca0b567753b0eff0c8b8ac2e01b35ae497742b
Arthur Bulle <arthurbulle@MacBook-Air-de-Arthur.local>
2024-10-22 10:52:19 +0200

    maj

---
+++ src/view/manga_view.py
@@ -0,0 +1,35 @@
+# Importer
+from dao.manga_dao import rechercher_manga_par_titre
+from avis_view import ajouter_avis_view, lire_avis_view
+from collection_view import ajouter_a_collection_view
+
+
+
+def rechercher_manga_view(utilisateur_id):
+    titre = input("Entrez le titre du manga : ")
+    mangas = rechercher_manga_par_titre(titre)
+
+    if mangas:
+        for i, manga in enumerate(mangas, 1):
+            print(f"{i}. {manga['title']} (ID: {manga['mal_id']})")
+
+        choix = int(input("Choisissez un manga par numéro : "))
+        manga_id = mangas[choix - 1]['mal_id']
+
+        # Proposer le choix d'ajouter un avis, ajouter à une collection, ou lire les avis
+        print("\nQue souhaitez-vous faire avec ce manga ?")
+        print("1. Ajouter un avis")
+        print("2. Ajouter à une collection")
+        print("3. Lire les avis des autres utilisateurs")
+        action = input("Choisissez une action (1, 2 ou 3) : ")
+
+        if action == '1':
+            ajouter_avis_view(utilisateur_id, manga_id)  # Appel pour ajouter un avis
+        elif action == '2':
+            ajouter_a_collection_view(utilisateur_id, manga_id)  # Appel pour ajouter à une collection
+        elif action == '3':
+            lire_avis_view(manga_id)  # Appel pour lire les avis des autres utilisateurs
+        else:
+            print("Action invalide.")
+    else:
+        print("Aucun manga trouvé.")
