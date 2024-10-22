4ef8c5bf21a69985d577ae760f18b9365be7efb7
Léo <leo.gruchociak2@gmail.com>
2024-10-22 10:00:54 +0200

    avis manga dao

--- src/dao/avis_manga_dao.py
+++ src/dao/avis_manga_dao.py
@@ -7,98 +7,92 @@ from utils.log_decorator import log
 
 from dao.db_connection import DBConnection
 
-from business_object.avis import Avis
+from business_object.avis_manga import AvisManga
 
 
 class AvisMangaDao(metaclass=Singleton):
-    """Classe contenant les méthodes pour accéder aux Joueurs de la base de """
+    """Classe contenant les méthodes pour accéder aux avis des mangas de la base de """
     """données"""
 
     @log
-    def create_avis(self, id_avis, id_utilisateur, id_manga,
-                    contenu, note) -> Avis:
+    def create_avis(self, avis) -> AvisManga:
         """trouver un manga grace à son id
 
         Parameters
         ----------
-        id_manga : int
-            numéro id du manga que l'on souhaite trouver
+        avis : AvisManga
 
         Returns
         -------
-        manga : Manga
-            renvoie le manga que l'on cherche par id
+        created : bool
+            True si la création est un succès
+            False sinon
         """
         try:
             with DBConnection().connection as connection:
                 with connection.cursor() as cursor:
                     cursor.execute(
-                        "INSERT INTO avis_manga(id_avis, id_utilisateur,"
-                        "id_manga, contenu, note) VALUES                "
+                        "INSERT INTO avis_manga(id_avis, id_utilisateur, "
+                        "id_manga, contenu, note) VALUES                 "
                         "(%(id_avis)s, %(id_utilisateur)s, %(id_manga)s, "
-                        "%(contenu)s, %(note)s)             "
-                        "  RETURNING id_joueur;                                                ",
+                        " %(contenu)s, %(note)s)                         "
+                        "  RETURNING id_joueur;                          ",
                         {
-                            "id_avis": avis_manga.id_avis,
-                            "id_utilisateur": avis_manga.id_utilisateur,
-                            "id_manga": avis_manga.id_manga,
-                            "contenu": avis_manga.contenu,
-                            "note": avis_manga.note,
+                            "id_avis": avis.id_avis,
+                            "id_utilisateur": avis.id_utilisateur,
+                            "id_manga": avis.id_manga,
+                            "contenu": avis.contenu,
+                            "note": avis.note,
                         },
                     )
                     res = cursor.fetchone()
         except Exception as e:
             logging.info(e)
-            raise
 
-       created = False
+        created = False
         if res:
-            avis_manga.id_avis = res["id_avis"]
+            avis.id_avis = res["id_avis"]
             created = True
 
         return created
 
     @log
-    def UpdateAvis(self,id_avis, nouveau_contenu,
-        nouvelle_note) -> Avis:
-        """trouver un manga grace à son id
+    def UpdateAvis(self, avis) -> bool:
+        """Modifier un avis dans la base de données
 
         Parameters
         ----------
-        id_manga : int
-            numéro id du manga que l'on souhaite trouver
+        avis : AvisManga
 
         Returns
         -------
-        manga : Manga
-            renvoie le manga que l'on cherche par id
+        created : bool
+            True si la modification est un succès
+            False sinon
         """
+        res = None
+
         try:
             with DBConnection().connection as connection:
                 with connection.cursor() as cursor:
                     cursor.execute(
-                        "INSERT INTO avis_manga(id_avis, id_utilisateur,"
-                        "id_manga, contenu, note) VALUES                "
-                        "(%(id_avis)s, %(id_utilisateur)s, %(id_manga)s, "
-                        "%(contenu)s, %(note)s)             "
-                        "  RETURNING id_joueur;                                                ",
+                        "UPDATE avis                                      "
+                        "   SET id_avis        = %(id_avis)s,             "
+                        "       id_utilisateur = %(id_utilisateur)s,      "
+                        "       id_manga       = %(id_manga)s,            "
+                        "       contenu        = %(contenu)s,             "
+                        "       note           = %(note)s                 "
+                        " WHERE id_joueur = %(id_joueur)s;                ",
                         {
-                            "id_avis": avis_manga.id_avis,
-                            "id_utilisateur": avis_manga.id_utilisateur,
-                            "id_manga": avis_manga.id_manga,
-                            "contenu": avis_manga.contenu,
-                            "note": avis_manga.note,
+                            "id_avis": avis.id_avis,
+                            "id_utilisateur": avis.id_utilisateur,
+                            "id_manga": avis.id_manga,
+                            "contenu": avis.contenu,
+                            "note": avis.note
                         },
                     )
-                    res = cursor.fetchone()
+                    res = cursor.rowcount
         except Exception as e:
             logging.info(e)
-            raise
 
-       created = False
-        if res:
-            avis_manga.id_avis = res["id_avis"]
-            created = True
-
-        return created UpdateAvis(id_avis, nouveau_contenu,
-nouvelle_note)
+        return res == 1
