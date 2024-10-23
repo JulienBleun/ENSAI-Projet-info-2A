dc133422c57fd1423494c33db417a15d3cfc1c20
Alexandre Osemont <aosemont@gmail.com>
2024-10-23 14:52:10 +0200

    a

--- src/service/manga_service.py
+++ src/service/manga_service.py
@@ -14,10 +14,6 @@ class MangaService:
     @log
     def consulter_manga(self, id_manga) -> Manga:
 
-<<<<<<< HEAD
         return MangaDao().trouver_par_id(id_manga)
 
 
-=======
-        return MangaDao().read_avis(id_manga)
->>>>>>> 3db1a9481357d47a6a97b24d36a3af611e597f61
