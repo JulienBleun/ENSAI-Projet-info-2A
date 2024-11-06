d770e04ed03860ff3bd4e31fefceb9a59d8155ed
Arthur Bulle <arthurbulle@MacBook-Air-de-Arthur.local>
2024-11-06 09:55:57 +0100

    solve pb

--- src/service/utilisateur_service.py
+++ src/service/utilisateur_service.py
@@ -1,24 +1,47 @@
-
-from tabulate import tabulate
-
+import tabulate
 from src.utils.log_decorator import log
-
-
-from src.business_object.utilisateur import utilisateur
+from src.business_object.utilisateur import Utilisateur
 from src.dao.utilisateur_dao import UtilisateurDao
 
-
 class UtilisateurService:
-    """Classe contenant les méthodes de service de l utilisateur """
-    @log
-    def inscription(self, utilisateur):
-        nouveau_utilisateur = utilisateur(
-            id_utilisateur=utilisateur.id_utilisateur,
-            nom=utilisateur.nom,
-            mdp=utilisateur.mdp
-        )
-    return nouveau_utilisateur if UtilisateurDao().add_Utilisateur(nouveau_utilisateur) else None
+    """Classe contenant les méthodes de service de l'utilisateur."""
 
+    @log
+    def inscription(self, nom, prenom, pseudo, email, mot_de_passe):
+        """Inscription d'un nouvel utilisateur.
+
+        Paramètres :
+        ------------
+        nom : str
+            Nom de l'utilisateur.
+        prenom : str
+            Prénom de l'utilisateur.
+        pseudo : str
+            Pseudo de l'utilisateur.
+        email : str
+            Email de l'utilisateur.
+        mot_de_passe : str
+            Mot de passe de l'utilisateur.
+
+        Retourne :
+        ----------
+        Utilisateur ou None : L'utilisateur créé ou None si l'inscription a échoué.
+        """
+
+        # Crée une instance d'utilisateur avec les informations fournies
+        nouveau_utilisateur = {
+            "nom": nom,
+            "prenom": prenom,
+            "pseudo": pseudo,
+            "email": email,
+            "mot_de_passe": mot_de_passe,
+        }
+
+        # Ajoute l'utilisateur via le DAO et renvoie le nouvel utilisateur s'il a été ajouté avec succès
+        if UtilisateurDao().add_utilisateur(nouveau_utilisateur):
+            return nouveau_utilisateur
+        else:
+            return None
 
 
 
