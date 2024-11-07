134f0c77d90d5ee725fdf46a21f1fe3087aad90d
Léo Gruchociak <leo.gruchociak@eleve.ensai.fr>
2024-11-07 15:26:25 +0100

    utilisateur service modif

--- src/service/utilisateur_service.py
+++ src/service/utilisateur_service.py
@@ -2,22 +2,27 @@
 from tabulate import tabulate
 
 from src.utils.log_decorator import log
+from utils.mdp_utils import hacher_mot_de_passe
 
-
-from src.business_object.utilisateur import utilisateur
+from src.business_object.utilisateur import Utilisateur
 from src.dao.utilisateur_dao import UtilisateurDao
 
 
 class UtilisateurService:
     """Classe contenant les méthodes de service de l utilisateur """
     @log
-    def inscription(self, utilisateur):
-        nouveau_utilisateur = utilisateur(
-            id_utilisateur=utilisateur.id_utilisateur,
-            nom=utilisateur.nom,
-            mdp=utilisateur.mdp
+    def inscription(self, nom, prenom, pseudo, email, mdp):
+        """"Création d'un utilisateur"""
+
+        nouveau_utilisateur = Utilisateur(
+            nom=nom,
+            prenom=prenom,
+            pseudo=pseudo,
+            email=email,
+            mdp=hacher_mot_de_passe(mdp, pseudo)
         )
-    return nouveau_utilisateur if UtilisateurDao().add_Utilisateur(nouveau_utilisateur) else None
+        return nouveau_utilisateur if UtilisateurDao().add_Utilisateur(
+               nouveau_utilisateur) else None
 
 
 
