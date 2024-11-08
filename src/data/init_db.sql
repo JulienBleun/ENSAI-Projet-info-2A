8bda416682f2398d7ff01c975045f488f0dfc188
Léo <leo.gruchociak2@gmail.com>
2024-11-08 17:24:53 +0100

    manga serial

--- src/data/init_db.sql
+++ src/data/init_db.sql
@@ -32,7 +32,7 @@ CREATE TABLE tp.collection (
 
 DROP TABLE IF EXISTS tp.manga CASCADE;
 CREATE TABLE tp.manga (
-    id_manga PRIMARY KEY,
+    id_manga SERIAL PRIMARY KEY,
     titre VARCHAR(400),
     descript TEXT
 );
