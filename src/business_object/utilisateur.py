7c35e479eabfd9317db85b642325b8e6ada3d8ab
Arthur Bulle <arthurbulle@MacBook-Air-de-Arthur.local>
2024-11-21 10:01:38 +0100

    user business obj

--- src/business_object/utilisateur.py
+++ src/business_object/utilisateur.py
@@ -1,34 +1,40 @@
-
-
-class Utilisateur():
+class Utilisateur:
     """
-    Classe représentant l' utilisateur.
+    Classe représentant l'utilisateur.
 
-    Parametres :
+    Paramètres :
     ------------
 
-    id : int
+    id_utilisateur : int
         L'identifiant unique associé à l'utilisateur.
 
     nom : str
+        Nom de l'utilisateur.
 
     prenom : str
+        Prénom de l'utilisateur.
 
     pseudo : str
+        Pseudo de l'utilisateur.
 
     email : str
+        Adresse email de l'utilisateur.
 
-    mot_de_passe : str
+    mdp : str
+        Mot de passe haché de l'utilisateur.
+
+    sel : bytes
+        Sel utilisé pour hacher le mot de passe.
     """
 
     def __init__(self, nom: str, prenom: str,
-                 pseudo: str,
-                 email: str, mdp: str, id_utilisateur=None
-                 ):
-
+                 pseudo: str, email: str, 
+                 mdp: str, sel: bytes = None, 
+                 id_utilisateur: int = None):
         self.id_utilisateur = id_utilisateur
         self.nom = nom
         self.prenom = prenom
         self.pseudo = pseudo
         self.email = email
         self.mdp = mdp
+        self.sel = sel  # Ajout du sel
